014283a58b43c015a60680ba1992b785315bf970
Bky567 <99029776+Bky567@users.noreply.github.com>
2022-02-04 22:39:54 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -441,11 +441,11 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
             SubmitResults();
         }
 
-        if (cps <= 5) return '试着好好练一下？';
+        if (cps <= 5) return '妈咪继续加油哦~';
         if (cps <= 8) return 'TCL';
         if (cps <= 10)  return 'TQL';
-        if (cps <= 15) return '您';
-        return '人？';
+        if (cps <= 15) return '太棒了妈咪！';
+        return '太棒了妈咪！';
     }
 
     function toStr(obj) {
